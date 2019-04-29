c29f9d59a0adc5a6b7117b8380ce2c11b1cdd886
Chandler Lucius <clucius@preludedynamics.com>
2019-04-29 15:55:07 -0500

    -Updated tests for DashboardWebSocket.java -Removed non wokring DemoApplicationTests.java file

--- src/test/java/com/utils/dashboard/DashboardWebSocketTest.java
+++ src/test/java/com/utils/dashboard/DashboardWebSocketTest.java
@@ -1,10 +1,24 @@
 package com.utils.dashboard;
 
+import java.io.File;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class DashboardWebSocketTest {
 
-    // @Test
-    // public void CopyScript_
+    @Test
+    public void copyExecScriptToTempDir_ValidInputOutputPath_FileExistsAndExecutable() {
+        DashboardWebSocket dashboardWebSocket = new DashboardWebSocket(true);
+        File tempFile = dashboardWebSocket.copyExecScriptToTempDir("/sh/ServerStats.sh", "ServerStats.sh");
+        Assert.assertTrue("Expected file to have been copied to temp directory.", tempFile.exists());
+        Assert.assertTrue("Expected file to be executable.", tempFile.canExecute());
+    }
+
+    @Test
+    public void copyExecScriptToTempDir_InvalidInputOutputPath_FileDoesNotExist() {
+        DashboardWebSocket dashboardWebSocket = new DashboardWebSocket(true);
+        File tempFile = dashboardWebSocket.copyExecScriptToTempDir("/sh/NotValid.sh", "NotValid.sh");
+        Assert.assertFalse("Expected file to not have been copied to temp directory.", tempFile.exists());
+    }
 
 }
