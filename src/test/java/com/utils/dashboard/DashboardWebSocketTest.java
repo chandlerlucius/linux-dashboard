2d7db174b1d6a4a53a33905c049e7533ff091ae6
Chandler Lucius <clucius@chandlerlucius.com>
2019-05-01 21:10:33 -0500

    -Updated default constructor -Changed methods to create file in the function

--- src/test/java/com/utils/dashboard/DashboardWebSocketTest.java
+++ src/test/java/com/utils/dashboard/DashboardWebSocketTest.java
@@ -7,21 +7,24 @@ import org.junit.Assert;
 public class DashboardWebSocketTest {
 
     public DashboardWebSocketTest() {
+        // This constructor is intentionally empty. Nothing special is needed here.
     }
 
     @Test
     public void copyExecScriptToTempDir_ValidInputOutputPath_FileExistsAndExecutable() {
         final DashboardWebSocket dashboardWebSocket = new DashboardWebSocket(true);
-        final File tempFile = dashboardWebSocket.copyExecScriptToTempDir("/sh/ServerStats.sh", "ServerStats.sh");
-        Assert.assertTrue("Expected file to have been copied to temp directory.", tempFile.exists());
-        Assert.assertTrue("Expected file to be executable.", tempFile.canExecute());
+        final String filePath = dashboardWebSocket.copyExecScriptToTempDir("/sh/ServerStats.sh", "ServerStats.sh");
+        final File file = new File(filePath);
+        Assert.assertTrue("Expected file to have been copied to temp directory.", file.exists());
+        Assert.assertTrue("Expected file to be executable.", file.canExecute());
     }
 
     @Test
     public void copyExecScriptToTempDir_InvalidInputOutputPath_FileDoesNotExist() {
         final DashboardWebSocket dashboardWebSocket = new DashboardWebSocket(true);
-        final File tempFile = dashboardWebSocket.copyExecScriptToTempDir("/sh/NotValid.sh", "NotValid.sh");
-        Assert.assertFalse("Expected file to not have been copied to temp directory.", tempFile.exists());
+        final String filePath = dashboardWebSocket.copyExecScriptToTempDir("/sh/NotValid.sh", "NotValid.sh");
+        final File file = new File(filePath);
+        Assert.assertFalse("Expected file to not have been copied to temp directory.", file.exists());
     }
 
 }
