03675cf944497534f017119ee380c54fdf661ce7
Chandler Lucius <clucius@chandlerlucius.com>
2019-05-01 21:07:41 -0500

    -Updated Application.java to change args to final

--- src/main/java/com/utils/dashboard/Application.java
+++ src/main/java/com/utils/dashboard/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan
 public abstract class Application {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
