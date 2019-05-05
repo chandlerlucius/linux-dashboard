cb9e1b4319bab89c9eba9007594960fec0cf15c2
Chandler Lucius <clucius@chandlerlucius.com>
2019-05-05 13:55:16 -0500

    -Updated js test to include more descriptions and remove commented out code -Updated package.json to have accurate description

--- src/test/js/websocket.test.js
+++ src/test/js/websocket.test.js
@@ -1,16 +1,11 @@
-// let assert = require('assert');
-// let websocket = require('../../main/resources/static/js/websocket.js')
-
-// describe('Escape HTML', function() {
-//   describe('when escapeHTML is called', function() {
-//     it('should convert all the unsafe HTML characters into HTML entities', function() {
-//       assert.equal('&amp;&lt;&gt;&quot;&apos;', websocket.escapeHTML('&<>"\''));
-//     });
-//   });
-// });
-
 const websocket = require('../../main/resources/static/js/websocket')
 
-test('should convert all the unsafe HTML characters into HTML entities', function() {
-    expect(websocket.escapeHTML('&<>"\'')).toBe('&amp;&lt;&gt;&quot;&apos;');
+describe('Escape HTML Function - escapeHTML()', function () {
+    it('should convert all the unsafe HTML characters into HTML entities', function () {
+        expect(websocket.escapeHTML('&<>"\'')).toBe('&amp;&lt;&gt;&quot;&apos;');
+    });
+
+    it('should leave all the safe HTML characters the way they are', function () {
+        expect(websocket.escapeHTML('qwertyuiopasdfghjklzxcvbnm')).toBe('qwertyuiopasdfghjklzxcvbnm');
+    });
 });
