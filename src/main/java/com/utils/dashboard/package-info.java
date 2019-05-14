6e4d72adc2da9e7e726506318359564719c0de2b
Chandler Lucius <clucius@chandlerlucius.com>
2019-05-13 19:40:13 -0500

    -Add package info classes to fill out javadoc

---
+++ src/main/java/com/utils/dashboard/package-info.java
@@ -0,0 +1,11 @@
+
+/**
+ * Source classes used to run the embedded undertow server and websocket communications.
+ * <p>
+ * These classes contain the meat of the dashboard code.
+ *
+ * @author Chandler Lucius
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+package com.utils.dashboard;
---
+++ src/test/java/com/utils/dashboard/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ * Test classes used to test the embedded undertow server and websocket communications.
+ *
+ * @author Chandler Lucius
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+package com.utils.dashboard;
