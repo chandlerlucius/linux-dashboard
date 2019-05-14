e4ac12040f32e847d2e62bdb52187c8bf0a60691
Chandler Lucius <clucius@chandlerlucius.com>
2019-05-13 19:37:46 -0500

    -Updated Http2Server.java to fix some code smells

--- src/main/java/com/utils/dashboard/Http2Server.java
+++ src/main/java/com/utils/dashboard/Http2Server.java
@@ -1,10 +1,8 @@
 package com.utils.dashboard;
 
 import javax.servlet.ServletException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.Undertow.Builder;
@@ -17,9 +15,20 @@ import io.undertow.servlet.api.ServletContainer;
 import io.undertow.servlet.core.ServletContainerImpl;
 import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
 
+/**
+ * This class is responsible for setting up and starting the embedded undertow server used for the
+ * application.
+ * 
+ * @author Chandler Lucius
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public final class Http2Server {
 
     private static final Logger LOG = LoggerFactory.getLogger(Http2Server.class);
+    private static final int BUFFER_SIZE = 100;
+    private static final int PORT = 8080;
+    private static final String IP = "0.0.0.0";
 
     private Http2Server() {
     }
@@ -27,14 +36,14 @@ public final class Http2Server {
     public static void main(final String[] args) {
         final PathHandler path = Handlers.path();
         final Builder builder = Undertow.builder();
-        builder.addHttpListener(8080, "0.0.0.0");
+        builder.addHttpListener(PORT, IP);
         builder.setHandler(path);
 
         final Undertow server = builder.build();
         server.start();
 
         final WebSocketDeploymentInfo wsDeploymentInfo = new WebSocketDeploymentInfo();
-        wsDeploymentInfo.setBuffers(new DefaultByteBufferPool(true, 100));
+        wsDeploymentInfo.setBuffers(new DefaultByteBufferPool(true, BUFFER_SIZE));
         wsDeploymentInfo.addEndpoint(WebSocket.class);
 
         final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
@@ -43,8 +52,10 @@ public final class Http2Server {
         deploymentInfo.setClassLoader(classLoader);
         deploymentInfo.addWelcomePage("index.html");
         deploymentInfo.setDeploymentName("linux-dashboard.war");
-        deploymentInfo.setResourceManager(new ClassPathResourceManager(classLoader, "com/utils/dashboard/"));
-        deploymentInfo.addServletContextAttribute(WebSocketDeploymentInfo.ATTRIBUTE_NAME, wsDeploymentInfo);
+        deploymentInfo.setResourceManager(
+                new ClassPathResourceManager(classLoader, "com/utils/dashboard/"));
+        deploymentInfo.addServletContextAttribute(WebSocketDeploymentInfo.ATTRIBUTE_NAME,
+                wsDeploymentInfo);
 
         final ServletContainer container = new ServletContainerImpl();
         container.addDeployment(deploymentInfo);
