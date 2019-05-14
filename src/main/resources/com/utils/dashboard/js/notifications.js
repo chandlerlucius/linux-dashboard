e12b89916297abdf342a71a5dbafd8fafbba1c74
Chandler Lucius <clucius@chandlerlucius.com>
2019-05-13 23:10:29 -0500

    -Updated use of var with let and const -Reworked ternary to if statement

--- src/main/resources/com/utils/dashboard/js/notifications.js
+++ src/main/resources/com/utils/dashboard/js/notifications.js
@@ -1,22 +1,26 @@
-"use strict";
+'use strict';
 
-self.addEventListener("message", function (e) {
-    var data = e.data;
-    var text = 'High ' + data.title + ' - ' + data.value;
-    var frequency = Number.isInteger(parseInt(data.frequency, 10)) ?
-        parseInt(data.frequency, 10) : 15;
-    var elaspedMinutes = (Math.abs(new Date() -
+self.addEventListener('message', function (e) {
+    const data = e.data;
+    const text = `High ${data.title} - ${data.value}`;
+    const elaspedMinutes = (Math.abs(new Date() -
         new Date(data.notifiedDate)) / 1000) / 60;
+    let frequency;
+    if (Number.isInteger(parseInt(data.frequency, 10))) {
+        frequency = parseInt(data.frequency, 10);
+    } else {
+        frequency = 15;
+    }
     if (frequency < elaspedMinutes) {
         if (data.email) {
-            var content =
+            const content =
                 '{"sender":{"email":"clucius08@gmail.com"},' +
                 '"to":[{"email":"clucius08@gmail.com"}],' +
                 '"htmlContent":"<h1>' + text + '</h1>' +
                 '<br><br><span>Good luck!</span><br>' +
                 '<span>Linux Dashboard</span>",' +
                 '"subject":"' + text + '"}';
-            var xhr = new XMLHttpRequest();
+            const xhr = new XMLHttpRequest();
 
             xhr.withCredentials = true;
             xhr.onreadystatechange = function () {
