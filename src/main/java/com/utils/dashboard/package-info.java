fcf04aa293405f9d191db99b25d94c13c1cb3649
Chandler Lucius <clucius@chandlerlucius.com>
2019-06-27 17:02:30 -0500

    -Added another sentence of description

--- src/main/java/com/utils/dashboard/package-info.java
+++ src/main/java/com/utils/dashboard/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Source classes used to run the embedded undertow server and websocket communications.
+ * They are comprised of a Servlet Container Initialization class as well as a Java Websocket
+ * API endpoint.
  * <p>
  * These classes contain the meat of the dashboard code.
  *
