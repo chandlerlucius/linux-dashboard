1ee6233222d83b7fa566b6791be927775363d44c
Chandler Lucius <clucius@chandlerlucius.com>
2019-06-28 16:50:09 -0500

    -Added test code for HTTPS/HTTP2 startup

--- src/main/java/com/utils/dashboard/Http2Server.java
+++ src/main/java/com/utils/dashboard/Http2Server.java
@@ -1,11 +1,24 @@
 package com.utils.dashboard;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import javax.servlet.ServletException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.Undertow.Builder;
+import io.undertow.UndertowOptions;
 import io.undertow.server.DefaultByteBufferPool;
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.server.handlers.resource.ClassPathResourceManager;
@@ -16,8 +29,8 @@ import io.undertow.servlet.core.ServletContainerImpl;
 import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
 
 /**
- * This class is responsible for setting up and starting the embedded 
- * undertow server used for the application.
+ * This class is responsible for setting up and starting the embedded undertow server used for the
+ * application.
  * 
  * @author Chandler Lucius
  * @version 1.0.0
@@ -27,7 +40,8 @@ public final class Http2Server {
 
     private static final Logger LOG = LoggerFactory.getLogger(Http2Server.class);
     private static final int BUFFER_SIZE = 100;
-    private static final int PORT = 8080;
+    private static final int HTTP_PORT = 8080;
+    private static final int HTTPS_PORT = 8443;
     private static final String IP_ADDRESS = "0";
 
     private Http2Server() {
@@ -36,8 +50,37 @@ public final class Http2Server {
     public static void main(final String[] args) {
         final PathHandler path = Handlers.path();
         final Builder builder = Undertow.builder();
-        builder.addHttpListener(PORT, IP_ADDRESS);
+        //Add HTTP listener
+        builder.addHttpListener(HTTP_PORT, IP_ADDRESS);
         builder.setHandler(path);
+
+        String certificatePath = System.getProperty("certificate", "hey");
+        if (!certificatePath.isEmpty()) {
+            try (InputStream inputStream = new FileInputStream(certificatePath)) {
+                System.out.println(certificatePath);
+                CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+                X509Certificate certificate =
+                        (X509Certificate) certificateFactory.generateCertificate(inputStream);
+
+                TrustManagerFactory trustManagerFactory =
+                        TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+                keyStore.load(null);
+                keyStore.setCertificateEntry("caCert", certificate);
+                trustManagerFactory.init(keyStore);
+
+                SSLContext sslContext = SSLContext.getInstance("TLS");
+                sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+
+                //Add HTTPS listener and enable HTTP2
+                builder.addHttpsListener(HTTPS_PORT, IP_ADDRESS, sslContext);
+                builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true);
+            } catch (IOException | KeyStoreException | NoSuchAlgorithmException
+                    | KeyManagementException | CertificateException e) {
+                // LOG.error("Issue retrieving certificate and setting up HTTPS: ", e);
+                System.err.println(e);
+            }
+        }
 
         final Undertow server = builder.build();
         server.start();
