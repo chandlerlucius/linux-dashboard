412a79e25f6dbf79b797244faa3b070f4371ae5f
Chandler Lucius <clucius@chandlerlucius.com>
2019-07-24 20:20:47 -0500

    -Updated Http2Server.java to handle https and http -> https redirection -Updated Http2Server.java to pull https and keystore info from application.properties file

--- src/main/java/com/utils/dashboard/Http2Server.java
+++ src/main/java/com/utils/dashboard/Http2Server.java
@@ -2,6 +2,7 @@ package com.utils.dashboard;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
@@ -10,11 +11,18 @@ import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.Properties;
+import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import javax.servlet.ServletException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,12 +31,16 @@ import io.undertow.Undertow;
 import io.undertow.Undertow.Builder;
 import io.undertow.UndertowOptions;
 import io.undertow.server.DefaultByteBufferPool;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.server.handlers.resource.ClassPathResourceManager;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
 import io.undertow.servlet.api.ServletContainer;
 import io.undertow.servlet.core.ServletContainerImpl;
+import io.undertow.util.Headers;
+import io.undertow.util.StatusCodes;
 import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
 
 /**
@@ -43,11 +55,10 @@ public final class Http2Server {
 
     private static final Logger LOG = LoggerFactory.getLogger(Http2Server.class);
     private static final int BUFFER_SIZE = 100;
-    private static final int HTTP_PORT = 8080;
-    private static final int HTTPS_PORT = 8443;
-    private static final String IP_ADDRESS = "0.0.0.0";
+    private static final String HTTP_PORT = "8080";
+    private static final String HTTPS_PORT = "8443";
+    private static final String IP_ADDRESS = "0";
 
-    private static final String PROPERTIES_FILE_PROP = "properties.file";
     private static final String HTTP_PORT_PROP = "http.port";
     private static final String HTTPS_PORT_PROP = "https.port";
     private static final String KEYSTORE_FILE_PROP = "keystore.file";
@@ -56,85 +67,85 @@ public final class Http2Server {
     private Http2Server() {
     }
 
-    public static void main(final String[] args) {
-        // Determine location of jar
-        String propertiesFilePath = "";
+    public static void main(final String[] args) throws FileNotFoundException {
+        // Determine location of running code or jar
+        String propertiesPath = "";
         try {
             CodeSource codeSource = Http2Server.class.getProtectionDomain().getCodeSource();
-            File jarFile = new File(codeSource.getLocation().toURI().getPath());
-            String jarPath = jarFile.getParentFile().getPath();
-            File propertiesFile = new File(jarPath + "/application.properties");
+            File codeLocation = new File(codeSource.getLocation().toURI().getPath());
+            String codePath = codeLocation.getParentFile().getPath();
+            File propertiesFile = new File(codePath + "/application.properties");
             if (propertiesFile.exists()) {
-                propertiesFilePath = propertiesFile.getPath();
+                propertiesPath = propertiesFile.getPath();
             }
         } catch (URISyntaxException e) {
-            System.err.println(e);
+            LOG.error("Issue getting code/jar location: ", e);
         }
 
-        // Load properties from passed in argument file or jar path
+        // Load properties from application.properties file
         Properties properties = new Properties();
-        String propertiesPath = System.getProperty(PROPERTIES_FILE_PROP, propertiesFilePath);
-        if (propertiesPath != null && !propertiesPath.isEmpty()) {
+        if (!propertiesPath.isEmpty()) {
             try (InputStream inputStream = new FileInputStream(propertiesPath)) {
                 properties.load(inputStream);
             } catch (Exception e) {
-                // LOG.error("Issue retrieving certificate and setting up HTTPS: ", e);
-                System.err.println(e);
-            }
-        } else {
-            try (InputStream inputStream =
-                    Http2Server.class.getResourceAsStream("/application.properties")) {
-                properties.load(inputStream);
-            } catch (Exception e) {
-                // LOG.error("Issue retrieving certificate and setting up HTTPS: ", e);
-                System.err.println(e);
+                LOG.error("Issue reading properties file: ", e);
             }
         }
 
-        // Try to pull properties from command line arguments and then properties file
-        String httpProp =
-                System.getProperty(HTTP_PORT_PROP, properties.getProperty(HTTP_PORT_PROP));
-        int httpPort =
-                httpProp != null && !httpProp.isEmpty() ? Integer.parseInt(httpProp) : HTTP_PORT;
+        int httpPort = Integer.parseInt(properties.getProperty(HTTP_PORT_PROP, HTTP_PORT));
+        int httpsPort = Integer.parseInt(properties.getProperty(HTTPS_PORT_PROP, HTTPS_PORT));
+        String keystoreFile = properties.getProperty(KEYSTORE_FILE_PROP);
+        String keystorePassword = properties.getProperty(KEYSTORE_PASSWORD_PROP);
 
-        String httpsProp =
-                System.getProperty(HTTPS_PORT_PROP, properties.getProperty(HTTPS_PORT_PROP));
-        int httspPort = httpsProp != null && !httpsProp.isEmpty() ? Integer.parseInt(httpsProp)
-                : HTTPS_PORT;
-
-        String keystoreFile =
-                System.getProperty(KEYSTORE_FILE_PROP, properties.getProperty(KEYSTORE_FILE_PROP));
-        String keystorePassword = System.getProperty(KEYSTORE_PASSWORD_PROP,
-                properties.getProperty(KEYSTORE_PASSWORD_PROP));
+        if (httpPort == 0 || httpsPort == 0) {
+            LOG.error("Provide a valid http.port or https.port!");
+            System.exit(1);
+        }
 
         final PathHandler path = Handlers.path();
         final Builder builder = Undertow.builder();
-
-        builder.addHttpListener(httpPort, IP_ADDRESS);
         builder.setHandler(path);
 
-        if (keystoreFile != null && keystorePassword != null && !keystoreFile.isEmpty()
-                && !keystorePassword.isEmpty()) {
+        if (!keystoreFile.isEmpty() || !keystorePassword.isEmpty()) {
             try (InputStream inputStream = new FileInputStream(keystoreFile)) {
-                System.out.println(keystoreFile);
-                KeyStore keyStore = KeyStore.getInstance("JKS");
-                keyStore.load(inputStream, keystorePassword.toCharArray());
+                KeyStore keystore = KeyStore.getInstance("PKCS12");
+                keystore.load(inputStream, keystorePassword.toCharArray());
 
+                KeyManager[] keyManagers;
                 KeyManagerFactory keyManagerFactory =
                         KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-                keyManagerFactory.init(keyStore, keystorePassword.toCharArray());
+                keyManagerFactory.init(keystore, keystorePassword.toCharArray());
+                keyManagers = keyManagerFactory.getKeyManagers();
 
-                SSLContext sslContext = SSLContext.getInstance("TLS");
-                sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+                KeyStore truststore = null;
+                TrustManager[] trustManagers;
+                TrustManagerFactory trustManagerFactory =
+                        TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+                trustManagerFactory.init(truststore);
+                trustManagers = trustManagerFactory.getTrustManagers();
 
-                // Add HTTPS listener and enable HTTP2
-                builder.addHttpsListener(httspPort, IP_ADDRESS, sslContext);
-                // builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true);
-            } catch (IOException | UnrecoverableKeyException | KeyStoreException
-                    | NoSuchAlgorithmException | CertificateException | KeyManagementException e) {
-                // LOG.error("Issue retrieving certificate and setting up HTTPS: ", e);
-                System.err.println(e);
+                SSLContext sslContext;
+                sslContext = SSLContext.getInstance("TLSv1.2");
+                sslContext.init(keyManagers, trustManagers, null);
+
+                builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true);
+                builder.addHttpsListener(httpsPort, IP_ADDRESS, sslContext);
+
+                builder.addHttpListener(httpPort, IP_ADDRESS, new HttpHandler() {
+                    @Override
+                    public void handleRequest(HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().add(Headers.LOCATION,
+                                "https://" + exchange.getHostName() + ":" + httpsPort
+                                        + exchange.getRelativePath());
+                        exchange.setStatusCode(StatusCodes.TEMPORARY_REDIRECT);
+                    }
+                });
+            } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException
+                    | IOException | KeyManagementException | UnrecoverableKeyException e) {
+                LOG.error("Issue loading keystore or ssl context.", e);
             }
+        } else {
+            builder.addHttpListener(httpPort, IP_ADDRESS);
         }
 
         final Undertow server = builder.build();
