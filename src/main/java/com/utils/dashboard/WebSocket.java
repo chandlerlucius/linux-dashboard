1a306c453ffa44dd6762a2f7879e0df871fbbdbb
Chandler Lucius <clucius@chandlerlucius.com>
2019-09-17 10:05:32 -0500

    -Removed unused methods

--- src/main/java/com/utils/dashboard/WebSocket.java
+++ src/main/java/com/utils/dashboard/WebSocket.java
@@ -1,20 +1,19 @@
 package com.utils.dashboard;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
-import java.util.Timer;
-import java.util.TimerTask;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,24 +29,12 @@ import org.slf4j.LoggerFactory;
 public class WebSocket {
 
     private static final Logger LOG = LoggerFactory.getLogger(WebSocket.class);
-    private static final String RUN_SCRIPT = "gimme";
     private static final String SEP = File.separator;
-    private static final int BUFFER_SIZE = 1024;
-    private static final int ONE_SECOND = 1000;
+    private static final String RUN_SCRIPT = "gimme";
 
-    private String data = "";
-
-    public WebSocket() {
-        final Timer timer = new Timer();
-        timer.schedule(new RunScript(), 0, ONE_SECOND);
-    }
-
-    public String getData() {
-        return this.data;
-    }
-    
-    public void setData(final String data) {
-        this.data = data;
+    static {
+        copyScriptToTempDir("/sh/ServerStats.sh", "ServerStats.sh");
+        runServerScript();
     }
 
     @OnOpen
@@ -64,7 +51,8 @@ public class WebSocket {
     public void handleMessage(final String message, final Session session) {
         if (RUN_SCRIPT.equals(message)) {
             try {
-                session.getBasicRemote().sendText(this.getData());
+                final String result = getServerScriptResult();
+                session.getBasicRemote().sendText(result);
             } catch (IOException e) {
                 LOG.error("Issue sending data to websocket: ", e);
             }
@@ -76,21 +64,9 @@ public class WebSocket {
         LOG.error("Issue with websocket connection: ", throwable);
     }
 
-    public class RunScript extends TimerTask {
-
-        public RunScript() {
-            copyScriptToTempDir("/sh/ServerStats.sh", "ServerStats.sh");
-        }
-
-        @Override
-        public void run() {
-            final String result = runServerScript();
-            setData(result);
-        }
-    }
-    
-    public final String copyScriptToTempDir(final String inputFilePath, final String outputFileName) {
-        try (InputStream inputStream = this.getClass().getResourceAsStream(inputFilePath)) {
+    public static final String copyScriptToTempDir(final String inputFilePath,
+            final String outputFileName) {
+        try (InputStream inputStream = WebSocket.class.getResourceAsStream(inputFilePath)) {
             if (inputStream != null) {
                 final String tempDirectory = System.getProperty("java.io.tmpdir");
                 final File tempFile = new File(tempDirectory + SEP + outputFileName);
@@ -106,40 +82,30 @@ public class WebSocket {
         return "";
     }
 
-    private final String runServerScript() {
+    private static final String runServerScript() {
         try {
-            // Run server stats script
+            LOG.info("Run Script");
             final String tempDirectory = System.getProperty("java.io.tmpdir");
             final File tempFile = new File(tempDirectory + "/ServerStats.sh");
-            final ProcessBuilder processBuilder = new ProcessBuilder("./" + tempFile.getName());
+            final ProcessBuilder processBuilder = new ProcessBuilder(tempFile.getAbsolutePath());
             processBuilder.directory(new File(tempDirectory));
-            final Process process = processBuilder.start();
-            process.waitFor();
-
-            return getServerScriptResult(process);
+            processBuilder.start();
         } catch (IOException e) {
             LOG.error("Issue running script: ", e);
-        } catch (InterruptedException e) {
-            LOG.error("Issue running script: ", e);
-            Thread.currentThread().interrupt();
         }
         return "";
     }
 
-    private final String getServerScriptResult(final Process process) {
-        // Get output from script
-        try (InputStream inputStream = process.getInputStream()) {
-            final ByteArrayOutputStream result = new ByteArrayOutputStream();
-            final byte[] buffer = new byte[BUFFER_SIZE];
-            int length = inputStream.read(buffer);
-            while (length != -1) {
-                result.write(buffer, 0, length);
-                length = inputStream.read(buffer);
-            }
-            return result.toString(StandardCharsets.UTF_8.name());
+    private final String getServerScriptResult() {
+        String retVal = "";
+        try {
+            final String tempDirectory = System.getProperty("java.io.tmpdir");
+            final File tempFile = new File(tempDirectory + "/ServerStats.txt");
+            retVal = new String(Files.readAllBytes(tempFile.toPath()), StandardCharsets.UTF_8.name());
+            // LOG.info(retVal);
         } catch (IOException e) {
-            LOG.error("Issue getting output from script: ", e);
+            LOG.error("Issue reading script: ", e);
         }
-        return "";
+        return retVal;
     }
 }
